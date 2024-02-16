81598c42820c3fe820ada02d057ba437d06edff0
Volodymyr Prystaiko <73882405+PrystaikoVolodymyr@users.noreply.github.com>
2024-02-16 17:08:53 +0200

    add logic sign-up

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 
 const { PORT } = require('./config/config')
 const express = require("express");
+const cors = require('cors');
+
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/", require("./routes/index"));
 
