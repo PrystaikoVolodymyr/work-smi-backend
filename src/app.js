f753320b8f9ddaf2d2d4bf7a4b41f6e26a34b360
Volodymyr Prystaiko <73882405+PrystaikoVolodymyr@users.noreply.github.com>
2024-02-23 13:55:32 +0200

    add logic sign-up

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 const { PORT } = require('./config/config')
 const express = require("express");
 const cors = require('cors');
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
 
 const app = express();
 
@@ -11,7 +12,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+const options = {
+    swaggerDefinition: {
+        openapi: '3.0.3',
+        info: {
+            title: 'work-smi',
+            version: '1.0.0',
+        }
+    },
+    apis: ['./routes/*.js'],
+};
+
 app.use("/", require("./routes/index"));
+
+const swaggerSpec = swaggerJSDoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`);
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,7 +4,31 @@ const userController = require("../controllers/user.controller");
 router.get("/", (req, res) => {
   res.json("USERS");
 });
+
 router.post("/sign-up", userController.signUpUser);
+
+/**
+ * @swagger
+ * /users/sign-up-linkedin:
+ *  get:
+ *    tags:
+ *      - Users
+ *    summary: Sign-up user via Linkedin
+ *    parameters:
+ *       - name: code,
+ *         required: true
+ *         type: string
+ *         in: body
+ *       - name: role
+ *         required: true
+ *         type: string
+ *         in: body
+ *    produces:
+ *       application/json
+ *    responses:
+ *      200:
+ *        description: Success
+ */
 router.post("/sign-up-linkedin", userController.singUpLinkedin);
 router.get("/all", userController.getAllUsers);
 router.delete("/", userController.deleteUserById);
