3b06323b63c6d2ba4f88a8959a33009e49121666
Volodymyr Prystaiko <73882405+PrystaikoVolodymyr@users.noreply.github.com>
2024-02-27 13:17:49 +0200

    auth

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,26 @@
+const { decodeIdToken } = require('../service/firebase.service');
+
+module.exports = {
+    ROLE_ADMIN: 'admin',
+    ROLE_CANDIDATE: 'candidate',
+    ROLE_RECRUITER: 'recruiter',
+    auth(roles) {
+        return async (req, res, next) => {
+            const token = req.get('authorization');
+
+            if (token) {
+                decodeIdToken(token.slice(7))
+                    .then(async userData => {
+                            req.user = userData;
+                            if (userData.role === 'admin' || roles.includes(userData.role)) {
+                                return next();
+                            }
+                        next(new Error('Not authorized for this operation'));
+                    })
+                    .catch(err => next(err))
+            } else {
+                next(new Error('No token provided'));
+            }
+        }
+    }
+}
